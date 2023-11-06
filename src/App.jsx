baec723058582fe458cef1f9e051bf8f1e9eb654
Zlatan Bekric <zlaja@Zlatans-MacBook-Pro.local>
2023-11-06 18:59:19 +0100

    Update design and routes

--- src/App.jsx
+++ src/App.jsx
@@ -19,21 +19,22 @@ function App() {
   useEffect(() => {
     const isDark = JSON.parse(localStorage.getItem('isDark'));
     const isSidebarHidden = JSON.parse(localStorage.getItem('isSidebarHidden'));
-    dispatch(sharedSlice.actions.setTheme(isDark));
-    dispatch(sharedSlice.actions.setSidebarHidden(isSidebarHidden));
-  }, []);
+    dispatch(sharedSlice.actions.setTheme(!!isDark));
+    dispatch(sharedSlice.actions.setSidebarHidden(!!isSidebarHidden));
+  }, [dispatch]);
 
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route path="/new-dashboard" element={<NewDashboard />} />
-      <Route path="/dashboard/:id" element={<h1>Dashboard</h1>} />
-      <Route path="/dashboard/:id/edit" element={<EditDashboard />} />
-      <Route path="/dashboard/:id/column/:columnId" element={<h1>ColumnDashboard</h1>} />
-      <Route path="/dashboard/:id/column/:columnId/edit" element={<EditColumnDashboard />} />
-      <Route path="/dashboard/:id/column/:columnId/task/:taskId" element={<h1>Task</h1>} />
-      <Route path="/new-task-dashboard/:id/column/:columnId/task" element={<NewTaskDashboard />} />
-      <Route path="/edit-task-dashboard/:id/column/:columnId/task/:taskId/edit" element={<EditTaskDashboard />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/new-dashboard" element={<NewDashboard />} />
+        <Route path="/dashboard/:id" element={<h1>Dashboard</h1>} />
+        <Route path="/dashboard/:id/edit" element={<EditDashboard />} />
+        <Route path="/dashboard/:id/column/:columnId" element={<h1>ColumnDashboard</h1>} />
+        <Route path="/dashboard/:id/column/:columnId/edit" element={<EditColumnDashboard />} />
+        <Route path="/dashboard/:id/column/:columnId/task/:taskId" element={<h1>Task</h1>} />
+        <Route path="/new-task-dashboard/:id/column/:columnId/task" element={<NewTaskDashboard />} />
+        <Route path="/edit-task-dashboard/:id/column/:columnId/task/:taskId/edit" element={<EditTaskDashboard />} />
+      </Route>
     </Routes>
   );
 }
